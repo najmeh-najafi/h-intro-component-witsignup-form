a6b905e4d2333b96edbbbcfc55cdbd8bc2898c94
najmeh-najafi <84215276+najmeh-najafi@users.noreply.github.com>
2023-02-05 09:18:37 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const form = document.querySelector("#form");
 const button=document.querySelector(".form__submit");
+const p2e = s => s.replace(/[۰-۹]/g, d => '۰۱۲۳۴۵۶۷۸۹'.indexOf(d))
 const passwordInput = document.querySelector(".form__Password");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -7,5 +8,6 @@ form.addEventListener("submit", (e) => {
 passwordInput.addEventListener("input",(e)=>{
     alert(e.target.value)
     alert(isNaN(e.target.value))
+    alert(p2e(e.target.value))
 })
   
